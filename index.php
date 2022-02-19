e2f65bc2925b92e68bfd895b417bae90f88fbfbc
rondluca <rdjgl24@gmail.com>
2022-02-19 15:25:57 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$a = 3;
+$b = 4;
+
+$h = sqrt(pow($a,2)+pow($b,2));
+
+echo "<h2 align='center'>Datos de el triangulo</h2>";
+echo "<h2 align='center'>Lado A: " . $a ." cm</h2>";
+echo "<h2 align='center'>Lado B: " . $b ." cm</h2>";
+echo "<h2 align='center'>Hipotenusa del triangulo: " . $h ." cm</h2>";
